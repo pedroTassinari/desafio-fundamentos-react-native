ec0614f792f97ccb4bb59cb534faee4a60f278d8
Pedro Tassinari <pedro.tassinari99@gmail.com>
2020-10-16 14:42:48 -0300

    iniciando projeto

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StatusBar, TextInput } from 'react-native';
+
+const App: React.FC = () => (
+  <View>
+    <TextInput>Here</TextInput>
+  </View>
+);
+
+export default App;
